03678ae4e20787b51194e6693241de47c94965cd
octop162 <octop162@icloud.com>
2021-08-23 23:20:25 +0900

    add tests

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -3,5 +3,10 @@ from playwright.sync_api import Page
 
 def test_wikipedia_title(page: Page):
     page.goto("https://wikipedia.org")
-    page.screenshot(path="screenshots/test.png")
+    page.screenshot(path="screenshots/wikipedia.png")
+    assert "Wikipedia" in page.title()
+
+def test_google_title(page: Page):
+    page.goto("https://google.com")
+    page.screenshot(path="screenshots/google.png")
     assert "Wikipedia" in page.title()
